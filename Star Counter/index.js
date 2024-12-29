5614f3f339021426c143637bbc40629a6a480488
Rajpandey7709 <rajdhanishpandey@gmail.com>
2024-12-29 10:02:46 +0530

    index.js

--- Star Counter/index.js
+++ Star Counter/index.js
@@ -1,8 +1,4 @@
-// document.getElementById("count-el").innerText = 5
 
-// change the count-el in the HTML to reflect the new count
-
-// camelCase
 let countEl = document.getElementById("count-el")
 console.log(countEl)// pass in arguments
 let savedCountEl = document.getElementById("saved-count")
